98933b375bb5bdb30390530e5bf85f7002a346f7
Littlenine <Himwdsj@gmail.com>
2025-11-17 18:59:49 +0800

    Add Defconfig to sh files
    Signed-off-by: Littlenine <Himwdsj@gmail.com>

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ export CROSS_COMPILE=aarch64-linux-gnu-
 export CCACHE_DIR=~/.ccache
 ccache -M 10G
 make mrproper
+make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} -o=OUT defconfig
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} -o=OUT Image dtbs modules
