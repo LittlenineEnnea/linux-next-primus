5c3bc3f128458ab311d31056df6d5429cd01f59a
Littlenine <Himwdsj@gmail.com>
2025-11-17 22:27:19 +0800

    Fix -o to O
    Signed-off-by: Littlenine <Himwdsj@gmail.com>

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ export CROSS_COMPILE=aarch64-linux-gnu-
 export CCACHE_DIR=~/.ccache
 ccache -M 10G
 make mrproper
-make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} -o=out defconfig
-make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} -o=out Image dtbs modules
+make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} O=out defconfig
+make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} O=out Image dtbs modules
