29a1d0bd389e5ba7c06c7b7b66a9e1f1aba4d0c1
Littlenine <Himwdsj@gmail.com>
2025-11-17 14:37:11 +0800

    Add SH files
    Signed-off-by: Littlenine <Himwdsj@gmail.com>

---
+++ build.sh
@@ -0,0 +1,6 @@
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CCACHE_DIR=~/.ccache
+ccache -M 10G
+make mrproper
+make -j$(nproc) ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} -o=OUT Image dtbs modules
---
+++ env.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev bc \
+                    git ccache qemu-user-static
+sudo apt install -y libncurses5-dev
+sudo apt install -y gcc-aarch64-linux-gnu
